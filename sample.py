3c63faaa143bccca9b6f99a839c6163059d6f857
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-17 17:57:00 +0530

    Update sample.py

--- sample.py
+++ sample.py
@@ -6,7 +6,7 @@ print(os.getcwd())
 import pandas as pd
 
 # Read the Excel file into a pandas DataFrame
-df = pd.read_excel('D:\demo\input.xlsx')
+df = pd.read_excel('D:/demo/input.xlsx')
 
 # Print the contents of the DataFrame
 print(df)
