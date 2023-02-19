5050a1f7198d41272d9182f4b99093031431d543
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-19 10:51:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,14 @@
+import sys
 import pandas as pd
-import openpyxl
 
-# Read input data from Excel file on GitHub
-url = 'https://raw.githubusercontent.com/bhavyakc09/sample1/main/input_data.xlsx'
-df = pd.read_excel(url)
+# Read command line argument for input column name
+input_column = sys.argv[1]
 
-# Process input data
-output_data = []
+# Read input Excel file
+df = pd.read_excel('input.xlsx')
+
+# Perform some operation on input column
 for index, row in df.iterrows():
-    # Read input values from row
-    input_value_1 = row['Input Column 1']
-    input_value_2 = row['Input Column 2']
-    
-    # Perform some operation on input values
-    output_value = input_value_1 + input_value_2
-    
-    # Append output value to output data list
-    output_data.append(output_value)
-
-# Write output data to Excel file on GitHub
-output_df = pd.DataFrame(output_data, columns=['Output Column'])
-output_url = 'https://raw.githubusercontent.com/bhavyakc09/sample1/main/output_data.xlsx'
-output_df.to_excel(output_url, index=False)
+    input_value = row[input_column]
+    # perform some operation on input value
+    print(input_value)
