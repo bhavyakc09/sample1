2691242ba2d83beb6034d1e081ffcfb91b37fbea
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-19 11:26:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,3 +10,5 @@ input_column = input("Enter the name of the column you want to use as input: ")
 for index, row in data_frame.iterrows():
     input_value = row[input_column]
     print(f"Row {index + 1} - {input_column}: {input_value}")
+    df = pd.read_excel('input_data.xlsx', engine='openpyxl')
+
