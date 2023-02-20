e06317349cb6a9ae5bcf41ba8a2ffc317decfcfb
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-20 12:49:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,18 +7,7 @@ print(sys.executable)
 data_frame = pd.read_excel('input_data.xlsx')
 
 # Prompt the user to enter the column name
-input_column = input("Enter the name of the column you want to use as input: ")
-
-#try and except blocks are used to catch the exception
-try:
-    	while True:
-       			 #input is assigned to a string variable check
-       			 check = raw_input('The input provided by the user is being read which is:')
-        			#the data assigned to the string variable is read
-        		 print 'Hello', check
-#EOFError exception is caught and the appropriate message is displayed
-except EOFError as x:
-    	print x
+input_column = input('Enter the name of the column you want to use as input: ')
 
 # Iterate over the rows and print the value in the input column
 for index, row in data_frame.iterrows():
