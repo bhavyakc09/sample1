5f003e68f580559ed4f2ac2ff12e1eb0f060c396
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-20 13:20:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import pandas as pd
 
-import sys
-print(sys.executable)
-
 # Load the Excel file
-data_frame = pd.read_excel('input_data.xlsx')
+input_file = input('Enter the path to the input Excel file: ')
+data_frame = pd.read_excel(input_file)
 
-# Prompt the user to enter the column name
+# Display the names of all columns in the file
+print('Available columns:')
+print(', '.join(data_frame.columns))
+
+# Prompt the user to enter the name of the column
 input_column = input('Enter the name of the column you want to use as input: ')
 
 # Iterate over the rows and print the value in the input column
 for index, row in data_frame.iterrows():
     input_value = row[input_column]
     print(f"Row {index + 1} - {input_column}: {input_value}")
-    df = pd.read_excel('input_data.xlsx', engine='openpyxl')
 
