6c7943213f74c7a83897ff4198f0383fdb807ee7
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-20 12:54:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,10 +15,6 @@ try:
     	while True:
        		 #input is assigned to a string variable check
         		check = raw_input('The input provided by the user is being read which is:')
-        		#the data assigned to the string variable is read
-#EOFError exception is caught and the appropriate message is displayed
-except EOFError as x:
-   	 print x
 
 # Iterate over the rows and print the value in the input column
 for index, row in data_frame.iterrows():
