cf15cbfbaac80fc7ba71a8fcc8b04f97dcbb8221
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-20 13:32:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import argparse
 import pandas as pd
 
-# Parse command-line arguments
-parser = argparse.ArgumentParser(description='Read an Excel file and print the values in a specified column.')
-parser.add_argument('--input_file', type=str, required=True, help='path to the input Excel file')
-parser.add_argument('--column_name', type=str, required=True, help='name of the column to read')
+# Define the command line arguments
+parser = argparse.ArgumentParser(description='Process input file')
+parser.add_argument('--input_file', type=str, help='path to input file')
+
+# Parse the command line arguments
 args = parser.parse_args()
 
 # Load the Excel file
 data_frame = pd.read_excel(args.input_file)
 
-# Iterate over the rows and print the value in the input column
+# Iterate over the rows and print the values in all columns
 for index, row in data_frame.iterrows():
-    input_value = row[args.column_name]
-    print(f"Row {index + 1} - {args.column_name}: {input_value}")
+    for col_name, col_value in row.iteritems():
+        print(f"Row {index + 1} - {col_name}: {col_value}")
+
 
 
