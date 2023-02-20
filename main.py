073e6b002ada5aecd9fd2359431b721f4ef55ed6
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-20 14:05:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import argparse
 import pandas as pd
 
-# Define the command line arguments
-parser = argparse.ArgumentParser(description='Process input file')
-parser.add_argument('--input_file', type=str, help='path to input file')
+def main(input_file, output_file):
+    # Load the input Excel file
+    data_frame = pd.read_excel(input_file)
 
-# Parse the command line arguments
-args = parser.parse_args()
+    # Iterate over the rows and print the values in all columns
+    for index, row in data_frame.iterrows():
+        for col_name, col_value in row.iteritems():
+            print(f"Row {index + 1} - {col_name}: {col_value}")
 
-# Check if input file argument was provided
-if not args.input_file:
-    print('Error: input file argument is missing.')
-    exit(1)
+    # Save the DataFrame to the output Excel file
+    data_frame.to_excel(output_file)
 
-# Load the Excel file
-data_frame = pd.read_excel(args.input_file)
+if __name__ == '__main__':
+    # Define the command line arguments
+    parser = argparse.ArgumentParser(description='Process input file')
+    parser.add_argument('--input_file', type=str, help='path to input file')
+    parser.add_argument('--output_file', type=str, help='path to output file')
 
-# Iterate over the rows and print the values in all columns
-for index, row in data_frame.iterrows():
-    for col_name, col_value in row.iteritems():
-        print(f"Row {index + 1} - {col_name}: {col_value}")
+    # Parse the command line arguments
+    args = parser.parse_args()
+
+    # Call the main function with the input and output file paths
+    main(args.input_file, args.output_file)
