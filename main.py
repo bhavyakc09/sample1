5217eca29e6e1293a69b861a5a531959daea6e25
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-20 13:00:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,6 @@ data_frame = pd.read_excel('input_data.xlsx')
 # Prompt the user to enter the column name
 input_column = input('Enter the name of the column you want to use as input: ')
 
-#EOFError program
-#try and except blocks are used to catch the exception
-try:
-    	while True:
-       		 #input is assigned to a string variable check
-        		check = raw_input('The input provided by the user is being read which is:')
-
 # Iterate over the rows and print the value in the input column
 for index, row in data_frame.iterrows():
     input_value = row[input_column]
