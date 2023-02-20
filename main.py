bec7249b3e496cc85447822c70a79e9aed8ac30d
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-20 14:21:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
-import argparse
-import pandas as pd
+import openpyxl
 
-def main(input_file, output_file):
-    # Load the input Excel file
-    data_frame = pd.read_excel(input_file)
+# open the Excel file
+wb = openpyxl.load_workbook('path/to/excel/file.xlsx')
 
-    # Iterate over the rows and print the values in all columns
-    for index, row in data_frame.iterrows():
-        for col_name, col_value in row.iteritems():
-            print(f"Row {index + 1} - {col_name}: {col_value}")
+# select the active sheet
+ws = wb.active
 
-    # Save the DataFrame to the output Excel file
-    data_frame.to_excel(output_file)
+# prompt the user to enter input
+user_input = input("Enter any input from the Excel file: ")
 
-if __name__ == '__main__':
-    # Define the command line arguments
-    parser = argparse.ArgumentParser(description='Process input file')
-    parser.add_argument('--input_file', type=str, help='path to input file')
-    parser.add_argument('--output_file', type=str, help='path to output file')
-
-    # Parse the command line arguments
-    args = parser.parse_args()
-
-    # Call the main function with the input and output file paths
-    main(args.input_file, args.output_file)
+# search for the input in the sheet
+for row in ws.iter_rows(values_only=True):
+    if user_input in row:
+        print(row)
