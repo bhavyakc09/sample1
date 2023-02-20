f656d62504aae277a00b5b43a9abaf95217514be
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-20 13:28:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
+import argparse
 import pandas as pd
 
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description='Read an Excel file and print the values in a specified column.')
+parser.add_argument('--input_file', type=str, required=True, help='path to the input Excel file')
+parser.add_argument('--column_name', type=str, required=True, help='name of the column to read')
+args = parser.parse_args()
+
 # Load the Excel file
-input_file = input('Enter the path to the input Excel file: ')
-data_frame = pd.read_excel(input_file)
-
-# Display the names of all columns in the file
-print('Available columns:')
-print(', '.join(data_frame.columns))
-
-# Prompt the user to enter the name of the column
-input_column = input('Enter the name of the column you want to use as input: ')
+data_frame = pd.read_excel(args.input_file)
 
 # Iterate over the rows and print the value in the input column
 for index, row in data_frame.iterrows():
-    input_value = row[input_column]
-    print(f"Row {index + 1} - {input_column}: {input_value}")
+    input_value = row[args.column_name]
+    print(f"Row {index + 1} - {args.column_name}: {input_value}")
+
 
