3fb48738aef9a1b848f028467f8ffabe107c839a
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-20 22:06:07 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,5 @@
-import my_module
-
-def test_function1():
-    assert my_module.function1(1, 2) == 3
-
-def test_function2():
-    assert my_module.function2(4) == 16
-    assert my_module.function2(-4) == 16
-
-def test_function3():
-    assert my_module.function3([1, 2, 3, 4]) == 2.5
-    assert my_module.function3([0, 0, 0]) == 0
-    
 def test_addition():
-    assert 2 + 2 == 4
+    assert 1 + 1 == 2
+
 
 
