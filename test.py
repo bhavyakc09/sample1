ea26a87d30266b5181f021bf1cbc149b4f4a1d0c
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-20 21:41:51 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+import my_module
+
+def test_function1():
+    assert my_module.function1(1, 2) == 3
+
+def test_function2():
+    assert my_module.function2(4) == 16
+    assert my_module.function2(-4) == 16
+
+def test_function3():
+    assert my_module.function3([1, 2, 3, 4]) == 2.5
+    assert my_module.function3([0, 0, 0]) == 0
+
