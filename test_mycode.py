62303bf8fbf103b9ae51c3fd50ed126b2caa93dc
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-21 10:45:41 +0530

    Update test_mycode.py

--- test_mycode.py
+++ test_mycode.py
@@ -2,7 +2,20 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from main import myfunc
+from mymodule import myfunc
+
+# define build() function
+def build():
+    # do something
+
+# define greet() function
+def greet():
+    # do something
+
+# call myfunc with input file path and user input
+result = myfunc('./input_data.xlsx', 'some input')
+print(result)
+
 
 def test_myfunc():
     assert myfunc(2, 3) == 5
