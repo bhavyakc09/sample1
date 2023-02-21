8bd26cbf4377c7c30a3afc289382090582564e73
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-21 11:00:22 +0530

    Update test_mycode.py

--- test_mycode.py
+++ test_mycode.py
@@ -8,6 +8,16 @@ from main import myfunc
 result = myfunc('./input_data.xlsx', 'some input')
 print(result)
 
+def test_myfunc(tmp_path):
+    # create a temporary input file
+    input_file = tmp_path / "test_input.xlsx"
+    wb = openpyxl.Workbook()
+    ws = wb.active
+    ws.cell(1, 1, "test value")
+    wb.save(input_file)
+
+    # test with valid input file and user input
+    assert myfunc(input_file, "test input") == "test value"
 
 def test_myfunc():
     assert myfunc(2, 3) == 5
