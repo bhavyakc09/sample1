f8b997df8ba2d22fca968f9f6a579081d6b3cb23
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-21 11:19:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,25 @@ import openpyxl
 def myfunc(a, b):
     return a + b
 
-def myfunc(input_file_path, user_input):
-    # open the Excel file
+def myfunc(input_file_path):
     wb = openpyxl.load_workbook(input_file_path)
+    sheet = wb.active
+    # Prompt the user to enter the input value from the Excel file
+    user_input = input(f"Enter any input from the Excel file (or press Enter to use the default value): ")
+    if user_input:
+        input_value = sheet[user_input].value
+    else:
+        input_value = sheet['A1'].value  # Use default value if user didn't enter a value
+    # Do some processing with the input value
+    result = input_value * 2
+    return result
     
 #def read_input_from_excel():
     # open the Excel file
     #wb = openpyxl.load_workbook('./input_data.xlsx')
 
     # select the active sheet
-    ws = wb.active
+    #ws = wb.active
 
     # prompt the user to enter input
     try:
