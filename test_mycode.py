5cdf07b9dc62f17640e0488260e627460baa301b
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-21 12:34:52 +0530

    Update test_mycode.py

--- test_mycode.py
+++ test_mycode.py
@@ -1,11 +1,13 @@
 import os
 import sys
 import openpyxl
+import pytest
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from main import myfunc
 
-def test_myfunc(tmp_path):
+def test_myfunc(tmp_path, monkeypatch):
     input_file = tmp_path / "input_data.xlsx"
     # create a test Excel file
     wb = openpyxl.Workbook()
@@ -15,7 +17,11 @@ def test_myfunc(tmp_path):
     sheet["A3"] = 3
     wb.save(input_file)
 
+    # set up input for the function
+    input_str = "A2\n"  # simulate user entering "A2" at the prompt
+    monkeypatch.setattr('builtins.input', lambda x: input_str)
+
     # test myfunc with the test input file
-    expected_result = 2
+    expected_result = 4
     result = myfunc(input_file)
     assert result == expected_result
