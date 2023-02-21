3dc54d7ad9f234bfbfc3fde0e0df7cd23ea4c555
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-21 10:03:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import openpyxl
 
-# open the Excel file
-wb = openpyxl.load_workbook('./input_data.xlsx')
+# define myfunc() function
+def myfunc():
+    # open the Excel file
+    wb = openpyxl.load_workbook('./input_data.xlsx')
 
-# select the active sheet
-ws = wb.active
+    # select the active sheet
+    ws = wb.active
 
-# prompt the user to enter input
-try:
-    user_input = input("Enter any input from the Excel file (or press Enter to use the default value): ") or "default value"
-except EOFError:
-    user_input = "default value"
+    # prompt the user to enter input
+    try:
+        user_input = input("Enter any input from the Excel file (or press Enter to use the default value): ") or "default value"
+    except EOFError:
+        user_input = "default value"
 
-# search for the input in the sheet
-for row in ws.iter_rows(values_only=True):
-    if user_input in row:
-        print(row)
+    # search for the input in the sheet
+    for row in ws.iter_rows(values_only=True):
+        if user_input in row:
+            return row
+    return None
