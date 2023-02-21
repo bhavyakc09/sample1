0962177111e1aef792e3c0bba31a179fe49c16cb
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-21 11:46:06 +0530

    Update test_mycode.py

--- test_mycode.py
+++ test_mycode.py
@@ -1,12 +1,9 @@
 import os
 import sys
+import openpyxl
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from main import myfunc
-
-# call myfunc with input file path and user input
-result = myfunc('./input_data.xlsx', 'some input')
-print(result)
 
 def test_myfunc(tmp_path):
     input_file = tmp_path / "test_input.xlsx"
@@ -19,6 +16,6 @@ def test_myfunc(tmp_path):
     wb.save(input_file)
 
     # test myfunc with the test input file
-    result = myfunc(input_file, 'some input')
+    expected_result = 2
+    result = myfunc(input_file)
     assert result == expected_result
-
