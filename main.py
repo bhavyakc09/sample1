1c8dc4afb1c06e69e0a2b49688fe5d56f235b965
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-21 11:45:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,22 @@
 import openpyxl
 
 # define myfunc() function
-def myfunc(a, b):
-    return a + b
-
-def myfunc(input_file_path):
-    wb = openpyxl.load_workbook(input_file_path)
-    sheet = wb.active
-    # Prompt the user to enter the input value from the Excel file
-    user_input = input(f"Enter any input from the Excel file (or press Enter to use the default value): ")
-    if user_input:
-        input_value = sheet[user_input].value
+def myfunc(input_file_path=None):
+    if input_file_path:
+        # read input from Excel file
+        wb = openpyxl.load_workbook(input_file_path)
+        sheet = wb.active
+        # Prompt the user to enter the input value from the Excel file
+        user_input = input(f"Enter any input from the Excel file (or press Enter to use the default value): ")
+        if user_input:
+            input_value = sheet[user_input].value
+        else:
+            input_value = sheet['A1'].value  # Use default value if user didn't enter a value
     else:
-        input_value = sheet['A1'].value  # Use default value if user didn't enter a value
+        # read input from user
+        input_value = input("Enter any input value: ")
+
     # Do some processing with the input value
     result = input_value * 2
+
     return result
-    
-#def read_input_from_excel():
-    # open the Excel file
-    #wb = openpyxl.load_workbook('./input_data.xlsx')
-
-    # select the active sheet
-    #ws = wb.active
-
-    # prompt the user to enter input
-    try:
-        user_input = input("Enter any input from the Excel file (or press Enter to use the default value): ") or "default value"
-    except EOFError:
-        user_input = "default value"
-
-    # search for the input in the sheet
-    for row in ws.iter_rows(values_only=True):
-        if user_input in row:
-            return row
-    return None
-
