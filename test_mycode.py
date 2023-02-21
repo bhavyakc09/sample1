7942a9a9a9ab5c1b1b933a0811b1494a718f0d56
bhavyakc09 <124873407+bhavyakc09@users.noreply.github.com>
2023-02-21 09:52:49 +0530

    Update test_mycode.py

--- test_mycode.py
+++ test_mycode.py
@@ -2,9 +2,9 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-import mycode
+from main import myfunc
 
 def test_myfunc():
-    assert mycode.myfunc(2, 3) == 5
-    assert mycode.myfunc(0, 0) == 0
-    assert mycode.myfunc(-1, 1) == 0
+    assert myfunc(2, 3) == 5
+    assert myfunc(0, 0) == 0
+    assert myfunc(-1, 1) == 0
